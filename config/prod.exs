f56967d53a7b222321b7db7ecca5aebbd5006fcf
Adam Zaninovich <adam.zaninovich@gmail.com>
2021-06-03 22:12:23 -0700

    set host for prod and force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
+  url: [host: "lv-chat-pia.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
